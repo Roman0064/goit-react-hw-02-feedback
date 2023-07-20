c3f48d62ef8ddce052ca795d8c286525cca37789
Roman0064 <122211211+Roman0064@users.noreply.github.com>
2023-07-20 14:31:29 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,9 @@
-import {Component} from "react";
+import React from 'react'
+import Feedback from './Feedback/Feedback'
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
+export const App = () => (
+  <>
+      <Feedback/>
+  </>
+);
   
-}
